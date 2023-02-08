f124b5cbd70736bc8f4f03630da3c3958b74f60b
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 20:33:40 -0500

    pass in onDeleteToy to ToyCard as prop

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer( { toys } ) {
+function ToyContainer( { toys, onDeleteToy} ) {
   return (
-    <div id="toy-collection">
+    <div id="toy-collection" >
       {/* Render the collection of ToyCards */}
       {toys?.map(toy => 
-        <ToyCard key={toy.id} toy={toy} />)}
+        <ToyCard key={toy.id} toy={toy} onDeleteToy={onDeleteToy} />)}
     </div>
   );
 }
