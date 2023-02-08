7141d0e544a72f48d72d47324ca1078684ffad8a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 20:35:03 -0500

    add onClick to delete btn, create handleDeleteClick with DELETE request, call onDeleteToy

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function ToyCard( { toy } ) {
+function ToyCard( { toy, onDeleteToy } ) {
 
   const {id, name, image, likes} = toy
 
+  // DELETE request
+  function handleDonateClick() {
+    fetch(`http://localhost:3001/toys/${id}`, {method: "DELETE"})
+    .then(res => res.json())
+    .then(() => onDeleteToy(toy))
+  }
+
   return (
-    <div className="card">
+    <div className="card" >
       <h2>{name}</h2>
       <img
         src={image}
@@ -14,7 +21,7 @@ function ToyCard( { toy } ) {
       />
       <p>{likes} Likes </p>
       <button className="like-btn">Like {likes}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="del-btn" onClick={handleDonateClick}>Donate to GoodWill</button>
     </div>
   );
 }
