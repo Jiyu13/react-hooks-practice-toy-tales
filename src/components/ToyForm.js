aaec7c25845b7d1a4d79b639432c6308106b94f6
Ziru Zou <jiyucoding@yahoo.com>
2023-02-07 20:20:41 -0500

    add onSubmit event, set formData state with initialValue, add value&onChange to each input, call handleSubmit with POST request

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm( { onAddToy } ) {
+
+  const initialValue = {
+    name: "",
+    image:"",
+    likes: 0
+  }
+
+  const [formData, setFormData] = useState(initialValue)
+
+  function handleOnchange(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const toyObj = {
+      name: formData.name,
+      image: formData.image,
+      likes: formData.likes
+    }
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify(toyObj)
+    })
+    .then(res => res.json())
+    .then(newToy => onAddToy(newToy))
+  }
+
+  
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={formData.name}
+          onChange={handleOnchange}
         />
         <br />
         <input
@@ -17,6 +58,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={formData.image}
+          onChange={handleOnchange}
         />
         <br />
         <input
